7efb222188724af9b5da54a721f5dcd5245127c4
vin06eet <vineetnayakm@gmail.com>
2025-04-05 19:57:35 +0530

    minor fixes

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ import uploadRoute from "./routes/upload.route.js"
 
 dotenv.config()
 const app = express()
+app.set('trust proxy', 1);
 const FRONTEND_ORIGIN = 'https://quizmaster-v3.vercel.app';
 
 app.use(cors({
