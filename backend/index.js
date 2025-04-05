07ceceea9b14a0b95b8a0dcb8ce5f7102e6ddaa2
vin06eet <vineetnayakm@gmail.com>
2025-04-05 15:22:50 +0530

    fixed a cors related issue

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import uploadRoute from "./routes/upload.route.js"
 
 dotenv.config()
 const app = express()
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ credentials: true }))
 
 app.use(bodyParser.json())
 app.use(cookieParser())
