d3f563be5f19c7e14dbbe77679c7e34fcb11a515
vin06eet <vineetnayakm@gmail.com>
2025-04-05 17:02:40 +0530

    fixed cookie related issue

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,15 @@ const FRONTEND_ORIGIN = 'https://quizmaster-sepia.vercel.app';
 app.use(cors({
   origin: FRONTEND_ORIGIN,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "Accept",
+    "Origin",
+    "Access-Control-Allow-Credentials"
+  ],
 }));
 
 app.options('*', cors({
